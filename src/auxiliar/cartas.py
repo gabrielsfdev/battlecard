befc08e854759686870642f1fe1cf874c8d82d21
gabrielsfdev <ferreirawdev@gmail.com>
2023-12-21 17:21:54 -0300

    implementado a classe que cria as cartas e uma função para criar as cartas

--- src/auxiliar/cartas.py
+++ src/auxiliar/cartas.py
@@ -1,13 +1,21 @@
 # ///// Coloque aqui o código referente a criação ou manipulação das cartas //////
 # Exemplo abaixo pode ser apagado, alterado ou feito alterações:
-"""
-class carta:
-    def __init__(self) -> None:
-        pass
+baralho = {}
+class Carta:
+    def __init__(self, nome, atk, defe, supertrunfo=False) -> None:
+        self.nome = nome
+        self.atk = atk
+        self.defe = defe
+        self.super_trunfo = supertrunfo
+
+    def add_carta(self):
+        baralho[f"Carta_{len(baralho) + 1}"] = {"nome": self.nome, "ataque": self.atk, "defesa": self.defe, "super_trunfo": self.super_trunfo}
+
+    def ver_baralho(self):
+        return baralho
 
     def atacar():
         pass
 
     def defender():
         pass
-"""
