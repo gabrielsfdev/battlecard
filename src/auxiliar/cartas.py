8fc53cd60630e671592439390d927a26259a60d3
gabrielsfdev <ferreirawdev@gmail.com>
2023-12-22 23:25:59 -0300

    Req2 e Req3 - Implementado os jogadores (req2), implementado mostrar cartas na mão (req3), pequena alteração em cartas, adicionado linha de código para resolver bug de acessar repositório (temporário), modificado path de diretorios

--- src/auxiliar/cartas.py
+++ src/auxiliar/cartas.py
@@ -1,6 +1,8 @@
-# ///// Coloque aqui o código referente a criação ou manipulação das cartas //////
-# Exemplo abaixo pode ser apagado, alterado ou feito alterações:
-baralho = {}
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../")))
+
+from data.cartas_exemplo import custom_deck
 class Carta:
     def __init__(self, nome, atk, defe, supertrunfo=False) -> None:
         self.nome = nome
@@ -9,13 +11,23 @@ class Carta:
         self.super_trunfo = supertrunfo
 
     def add_carta(self):
-        baralho[f"Carta_{len(baralho) + 1}"] = {"nome": self.nome, "ataque": self.atk, "defesa": self.defe, "super_trunfo": self.super_trunfo}
+        custom_deck[f"Carta_{len(custom_deck) + 1}"] = {
+                                                        "nome": self.nome, 
+                                                        "ataque": self.atk, 
+                                                        "defesa": self.defe, 
+                                                        "super_trunfo": self.super_trunfo
+                                                        }
 
     def ver_baralho(self):
-        return baralho
+        return custom_deck
 
     def atacar():
         pass
 
     def defender():
         pass
+
+if __name__ == "__main__":
+    carta1 = Carta("Teste", 25, 52)
+    carta1.add_carta()
+    print(carta1.ver_baralho())
